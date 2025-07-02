f745ca636422a6446e5e4671581bd5c789ee5ccf
breadleaf <bkhnapa@gmail.com>
2025-07-02 15:05:32 -0700

    add new blog post

--- posts/sourceMap.js
+++ posts/sourceMap.js
@@ -1,6 +1,12 @@
 export default function sourceMap() {
 	return [
 	[
+		'My proudest achievement',
+		'July 02, 2025 15:42 MDT',
+		'A short essay about graduating college',
+		'posts/html/02-07-2025_15-42-59.html',
+	],
+	[
 		'Credit where credit is due.',
 		'March 27, 2025 00:48 MDT',
 		'For my first post on my new page, it only felt right to credit some of my inspiration.',
